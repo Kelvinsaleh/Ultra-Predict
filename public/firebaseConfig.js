c70307a227dd4fa6dc9b5bba82a4c7b21fada1d6
Soccer <Knsalee@gmail.com>
2025-03-09 01:19:31 +0300

    Update firebaseConfig.js

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,12 +1,12 @@
-// firebaseConfig.js - Fixed
+// firebaseConfig.js - Fixed API Key & Config
 const firebaseConfig = {
-    apiKey: "ATzAsByPZa-Ft6vQAnEzqvqXJ5XPq9xTgB3A",
+    apiKey: "AIzaSyBp72a-Ft6vgAnEzqyvq5IXSpqvx8Tg03A",  // Correct API key
     authDomain: "ultrapredict-d60f4.firebaseapp.com",
     projectId: "ultrapredict-d60f4",
     storageBucket: "ultrapredict-d60f4.appspot.com",
-    messagingSenderId: "997625707900",
-    appId: "1:997625707900:web:78321d4ba50eb82574c329",
-    measurementId: "G-DV3AR7T38V"
+    messagingSenderId: "997625707020",  // Corrected sender ID
+    appId: "1:997625707020:web:78321d4ba50eb825374c29",  // Corrected App ID
+    measurementId: "G-DV33RT4T3W"
 };
 
 module.exports = firebaseConfig;
