01b120cf124468abb376ba19b5c968a983830e4a
Soccer <Knsalee@gmail.com>
2025-03-08 17:21:55 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+var serviceAccount = require("./config/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+console.log("Firebase Admin SDK initialized successfully!");
