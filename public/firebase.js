2360f3de5c7b055fb3a41c68f4d64529fe9c05d1
Soccer <Knsalee@gmail.com>
2025-03-09 01:18:16 +0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,10 +1,9 @@
-// firebase.js - Fixed
+// firebase.js - Fixed Firestore Initialization
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccount.json"); // Ensure this file exists
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://ultrapredict-d60f4-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
